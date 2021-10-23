9393f7102b78e6599d6807aaf0e332095dc0cf9d
usman182 <i190455@nu.edu.pk>
2021-10-23 09:45:04 +0500

    Commit

---
+++ Assignment2/src/Flight.java
@@ -0,0 +1,32 @@
+
+public class Flight {
+	
+	private String[] flightDates;
+	
+	
+	private Passenger[] passengers;
+	// Function to set Flight dates
+		public void setFlightDate()
+		{
+		
+	flightDates[0] = "23-10-2021 => Islamabad to Lahore => Dept Time: 8:00 AM => Arrival Time: 8:30 AM => Seat Type:First => Plane: PK211 => Fare Amount: 30,000 PKR/- => Total Seats: 50";
+	flightDates[1] = "23-10-2021 => Islamabad to Lahore => Dept Time: 10:00 AM => Arrival Time: 10:30 AM => Seat Type:Economy => Plane: PK212 => Fare Amount: 40,000 PKR/- => Total Seats: 60";
+	flightDates[2] = "23-10-2021 => Islamabad to Lahore => Dept Time: 12:00 PM => Arrival Time: 12:30 PM => Seat Type:Business => Plane: PK213 => Fare Amount: 50,000 PKR/- => Total Seats: 70";
+			
+			
+	flightDates[3] = "23-10-2021 => Islamabad to Karachi => Dept Time: 7:00 AM => Arrival Time: 11:00 AM => Seat Type:First => Plane: PK214 => Fare Amount: 100,000 PKR/- => Total Seats: 50";
+	flightDates[4] = "23-10-2021 => Islamabad to Karachi => Dept Time: 1:00 PM => Arrival Time: 4:00 PM => Seat Type:Economy => Plane: PK215 => Fare Amount: 110,000 PKR/- => Total Seats: 60";
+	flightDates[5] = "23-10-2021 => Islamabad to Karachi => Dept Time: 6:00 PM => Arrival Time: 9:00 PM => Seat Type:Business => Plane: PK216 => Fare Amount: 120,000 PKR/- => Total Seats: 70";
+		
+		
+	flightDates[6] = "23-10-2021 => Islamabad to Faisalabad => Dept Time: 5:00 AM => Arrival Time: 6:00 AM => Seat Type:First => Plane: PK217 => Fare Amount: 40,000 PKR/- => Total Seats: 50";
+	flightDates[7] = "23-10-2021 => Islamabad to Faisalabad => Dept Time: 6:00 AM => Arrival Time: 7:00 AM => Seat Type:Economy => Plane: PK218 => Fare Amount: 50,000 PKR/- => Total Seats: 60";
+	flightDates[8] = "23-10-2021 => Islamabad to Faisalabad => Dept Time: 7:00 AM => Arrival Time: 8:00 AM => Seat Type:Business => Plane: PK219 => Fare Amount: 60,000 PKR/- => Total Seats: 70";	
+		
+		}
+		
+		/*public void setPassenger(Passenger p) {			
+			passengers.add(p)
+		}*/
+
+}
