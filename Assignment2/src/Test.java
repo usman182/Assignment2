7907f1189de28017448c9f5710de1a320b55d5ae
usman182 <i190455@nu.edu.pk>
2021-11-17 14:46:26 +0500

    Commited

---
+++ Assignment2/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Greetings dear students");
+		System.out.println("Welcome to CI/CD");
+		System.out.println("An important phase of DevOps");
+	}
+
+}
